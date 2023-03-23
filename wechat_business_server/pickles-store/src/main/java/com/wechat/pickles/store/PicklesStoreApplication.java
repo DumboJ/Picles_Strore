e68bdabc31341ece003a47725c31280a0b6ecb89
DumboJ <dumbojxx@gmail.com>
2023-03-23 23:58:54 +0800

    init server code

---
+++ wechat_business_server/pickles-store/src/main/java/com/wechat/pickles/store/PicklesStoreApplication.java
@@ -0,0 +1,13 @@
+package com.wechat.pickles.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicklesStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PicklesStoreApplication.class, args);
+    }
+
+}
---
+++ wechat_business_server/pickles-store/src/test/java/com/wechat/pickles/store/PicklesStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wechat.pickles.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicklesStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
